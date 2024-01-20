0d9fb2d88e01f72a8a07695a875e0e43a8b7fc14
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-20 19:29:18 +0100

    feature: reimplement debug commands pause, step over, step into (#22)

--- packages/debug-worker/src/parts/Main/Main.js
+++ packages/debug-worker/src/parts/Main/Main.js
@@ -1,5 +1,14 @@
 import * as DebugStart from '../DebugStart/DebugStart.js'
+import * as DebugProvider from '../DebugProvider/DebugProvider.js'
 
 export const commandMap = {
   'Debug.start': DebugStart.debugStart,
+  'Debug.resume': DebugProvider.resume,
+  'Debug.pause': DebugProvider.pause,
+  'Debug.setPauseOnExceptions': DebugProvider.setPauseOnExceptions,
+  'Debug.stepOver': DebugProvider.stepOver,
+  'Debug.stepInto': DebugProvider.stepInto,
+  'Debug.stepOut': DebugProvider.stepOut,
+  'Debug.step': DebugProvider.step,
+  'Debug.evaluate': DebugProvider.evaluate,
 }
