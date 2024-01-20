0c7db754775bbaa543b6e698b59efea44d9ca54a
Le Vivilet <levivlet@pm.me>
2024-01-20 18:07:18 +0100

    refactor: remove unused code

--- packages/extension/src/parts/DevtoolsProtocolAnimation/DevtoolsProtocolAnimation.js
+++
@@ -1,9 +0,0 @@
-import * as DevtoolsCommandType from '../DevtoolsCommandType/DevtoolsCommandType.js'
-import { DevtoolsProtocolError } from '../DevtoolsProtocolError/DevtoolsProtocolError.js'
-
-export const enable = async (rpc, options) => {
-  const rawResult = await rpc.invoke(DevtoolsCommandType.AnimationEnable, options)
-  if ('error' in rawResult) {
-    throw new DevtoolsProtocolError(rawResult.error.message)
-  }
-}
--- packages/extension/src/parts/GetJson/GetJson.js
+++
@@ -1,7 +0,0 @@
-export const getJson = async (url) => {
-  const response = await fetch(url)
-  if (!response.ok) {
-    throw new Error(response.statusText)
-  }
-  return response.json()
-}
