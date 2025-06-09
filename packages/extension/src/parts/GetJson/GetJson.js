9fe01a1e562277a41faf4944c9e8f44ceac47b2c
Le Vivilet <levivlet@pm.me>
2025-06-09 22:38:33 +0200

    feature: close websocket connection after use

--- packages/extension/src/parts/GetJson/GetJson.js
+++ packages/extension/src/parts/GetJson/GetJson.js
@@ -1,11 +1,26 @@
 import * as DebugNodeUrl from '../DebugNodeUrl/DebugNodeUrl.js'
 
-export const getJson = async (...params) => {
+// const getJsonLocal = async (url) => {
+//   const response = await fetch(url)
+//   if (!response.ok) {
+//     throw new Error(response.statusText)
+//   }
+//   const json = await response.json()
+//   return json
+// }
+
+export const getJson = async (url) => {
+  // if (url && url.startsWith('http://localhost')) {
+  //   return getJsonLocal(url)
+  // }
+
   const nodePath = DebugNodeUrl.getDebugNodeUrl()
   // @ts-ignore
   const nodeRpc = await vscode.createNodeRpc({
     path: nodePath,
     name: 'Debug Worker',
   })
-  return nodeRpc.invoke('Ajax.getJson', ...params)
+  const json = await nodeRpc.invoke('Ajax.getJson', url)
+  await nodeRpc.dispose()
+  return json
 }
